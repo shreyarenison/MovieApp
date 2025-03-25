d2ce666cb7d916a9e8e8aaf0eb460139091b1ed5
shreyarenison <shreyarenison@gmail.com>
2025-03-25 19:22:31 -0400

    Modified AndroidManifest.xml, added item_movie.xml, modified strings.xml, added MovieAdapter (RecyclerView Adapter) in adapter package

--- app/src/main/java/com/example/movieapp/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/movieapp/adapter/MovieAdapter.java
@@ -9,10 +9,58 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.movieapp.MovieDetailsActivity;
+import com.example.movieapp.R;
+import com.example.movieapp.model.Movie;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 
-public class MovieAdapter {
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+    private Context context;
+    private List<Movie> movieList;
+
+    public MovieAdapter(Context context, List<Movie> movieList) {
+        this.context = context;
+        this.movieList = movieList;
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new MovieViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+        Movie movie = movieList.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.year.setText(movie.getYear());
+        holder.rating.setText(movie.getRating());
+        holder.studio.setText(movie.getStudio());
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, MovieDetailsActivity.class);
+            intent.putExtra("imdbID", movie.getImdbID());
+            context.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, year, rating, studio;
+
+        public MovieViewHolder(View view) {
+            super(view);
+            title = view.findViewById(R.id.title);
+            year = view.findViewById(R.id.year);
+            rating = view.findViewById(R.id.rating);
+            studio = view.findViewById(R.id.studio);
+        }
+    }
 }
