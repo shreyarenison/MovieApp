5e756fb8cd4fa5015d9bd5d64a7bdc046f450be2
shreyarenison <shreyarenison@gmail.com>
2025-03-25 18:51:45 -0400

    Modified build.gradle by importing Picasso in the dependencies

---
+++ app/src/main/java/com/example/movieapp/adapter/MovieAdapter.java
@@ -0,0 +1,18 @@
+package com.example.movieapp.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+
+public class MovieAdapter {
+}
